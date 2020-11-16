81716003698c16ca88c2ee4e7e2c1d4a7e93913f
Maura-P <74536217+Maura-P@users.noreply.github.com>
2020-11-16 10:51:44 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -29,6 +29,22 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
+        DATA_DIR = 'Homework2-Caltech101/101_ObjectCategories'
+
+# Prepare Pytorch train/test Datasets
+        #get classes
+        classes = [d.name for d in os.scandir(root) if d.is_dir() ]
+        classes.sort()
+        classes.pop(0)#drops Background Class
+        class_to_idx = {classes[i]: i for i in range(len(classes))}
+        
+        #create dataset
+        self.dataset=[]
+        with open('Homework2-Caltech101/'+ split +'.txt') as f:
+          for x in f:
+            c, imm=x.split('/')
+            if (c in class_to_idx):
+              self.dataset.append((pil_loader(root + '/'+x.split('\n')[0]), class_to_idx[c]))
 
     def __getitem__(self, index):
         '''
